e919adb4494f69fdcb5061e65961f3444f61dfb8
zhijun <galty687@gmail.com>
2018-08-24 13:50:53 +0800

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "新博客启用",
         "excerpt":"新博客 一直是一个爱好写作的人，在QZone，人人网，新浪博客，知乎等地方留下了很多文字，可是随着网络的发展，很多平台见见不用了，过往的文字也逐渐尘封，而且现有的博客平台一直有一种不受控的感觉，譬如新浪博客没法放代码，自己辛苦拍好的版面，一道新浪博客上就全乱了，每篇文章旁边的广告也是不胜其烦。 一直想搭建一个属于自己的博客站，记录自己的所思所想，若干年后回顾自己的生涯时能知道我当时这样思考过。今天终于下定决心，基于Jekyll 和 Minimal Mistakes 主题搭建了一个极简主义风的网站，以黑白两色为主，越简单越好。就主题而言，最后发现最耐看的还是配色最简单的，花花绿绿的主题当时看着爽，过了几个月再看自己都会觉得丑了。 ","categories": [],
         "tags": ["jekyll","life"],
-        "url": "/new_blog/",
+        "url": "http://localhost:4000/new_blog/",
         "teaser":null},{
         "title": "调教 Jekyll 博客",
         "excerpt":"Jekyll 带来了巨大的便利，使用的过程也充满着乐趣，遂决定把调教过程记录下来，备忘的同时，也给其他朋友提供一些参考。 启用 Disqus   在 Disqus 中provider               : \"disqus\" \"   disqus:    shortname            : \"gaozhijun\"强制Jekyll运行环境为production Jekyll本地运行时默认环境为Development，有一些插件需要在production环境下运行，如果需要使用production环境运行，可以按照如下命令运行。 JEKYLL_ENV=production bdle exec jekyll serveFavicon 想来一个酷酷的Favicon，发现这个在线工具不错： favicon.io ","categories": [],
         "tags": ["jekyll"],
-        "url": "/customizing_blog/",
+        "url": "http://localhost:4000/customizing_blog/",
         "teaser":null}]
